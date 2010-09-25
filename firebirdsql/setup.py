01923b135492c97523b6567d5d493c412ab18df2
Hajime Nakagami <nakagami@gmail.com>
2010-09-25 17:58:01 +0900

    Add short_description and url to setup.py

--- firebirdsql/setup.py
+++ firebirdsql/setup.py
@@ -1,3 +1,7 @@
 from distutils.core import setup
-setup(name='firebirdsql', version='0.1.0')
+setup(name='firebirdsql', 
+        version='0.1.0',
+        short_description = 'python3 dbapi for firebird rdbms.', 
+        url='http://github.com/nakagami/pyfirebirdsql/',
+)
 
