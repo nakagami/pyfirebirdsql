7f09f2bf62ee18b649373b952acc0425f03625cf
Hajime Nakagami <nakagami@gmail.com>
2010-09-26 19:59:11 +0900

    Add setup.py parameters.

--- firebirdsql/setup.py
+++ firebirdsql/setup.py
@@ -1,7 +1,19 @@
 from distutils.core import setup
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Topic :: Database',
+    'License :: OSI Approved :: BSD License',
+]
+
 setup(name='firebirdsql', 
         version='0.1.0',
-        short_description = 'python3 dbapi for firebird rdbms.', 
+        description = 'python3 dbapi for firebird rdbms.', 
         url='http://github.com/nakagami/pyfirebirdsql/',
+        classifiers=classifiers,
+        keyword=['Firebird'],
+        license='BSD',
+        author='Hajime Nakagami',
+        author_email='nakagami@gmail.com',
 )
 
