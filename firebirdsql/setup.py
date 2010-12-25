d2305c13b3686215b713135c9072f00cf9e4989f
Hajime Nakagami <nakagami@gmail.com>
2010-12-25 12:23:07 +0900

    vertion = 0.3.2

--- firebirdsql/setup.py
+++ firebirdsql/setup.py
@@ -7,7 +7,7 @@ classifiers = [
 ]
 
 setup(name='firebirdsql', 
-        version='0.3.1',
+        version='0.3.2',
         description = 'python dbapi for firebird rdbms.', 
         url='http://github.com/nakagami/pyfirebirdsql/',
         classifiers=classifiers,
