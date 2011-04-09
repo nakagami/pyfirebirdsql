47a4369ef3d65c6d355ab5a5b7ef78d329745e51
Hajime Nakagami <nakagami@gmail.com>
2011-04-09 20:26:43 +0900

    add name parameter in trace_start test script

--- misc/trace.py
+++ misc/trace.py
@@ -20,7 +20,7 @@ def debug_print(msg):
 
 def print_usage():
     print(sys.argv[0] + 
-                ' start|stop <trc_id>|suspend <trc_id>|resume <trc_id>|list')
+        ' start [name]|stop <trc_id>|suspend <trc_id>|resume <trc_id>|list')
 
 if len(sys.argv) < 2:
     print_usage()
@@ -28,8 +28,9 @@ if len(sys.argv) < 2:
 
 svc = firebirdsql.service_mgr(host=HOST, user=USER, password=PASS)
 if sys.argv[1] == 'start':
+    name = sys.argv[2] if len(sys.argv) == 3 else None
     cfg = open('/opt/firebird/fbtrace.conf').read()
-    svc.trace_start(cfg=cfg, callback=debug_print)
+    svc.trace_start(name=name, cfg=cfg, callback=debug_print)
 elif sys.argv[1] == 'stop':
     svc.trace_stop(id=sys.argv[2], callback=debug_print)
 elif sys.argv[1] == 'suspend':
