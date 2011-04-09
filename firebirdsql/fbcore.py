1d35c1e863c6a20df5a5f31bcbe115785063d0bd
Hajime Nakagami <nakagami@gmail.com>
2011-04-09 21:01:56 +0900

    major version number up

--- firebirdsql/fbcore.py
+++ firebirdsql/fbcore.py
@@ -22,7 +22,7 @@ def bs(byte_array):
     return ''.join([chr(c) for c in byte_array])
 
 DEBUG = False
-__version__ = '0.3.5'
+__version__ = '0.4.0'
 apilevel = '2.0'
 threadsafety = 1
 paramstyle = 'qmark'
