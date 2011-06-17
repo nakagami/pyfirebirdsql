92a3f2bfb662f8d4675bb98f04c1ae80061cad72
Hajime Nakagami <nakagami@gmail.com>
2011-06-17 20:13:25 +0900

    add a description.

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
+#!/usr/bin/env python
+""" firebirdsql package is a set of Firebird RDBMS bindings for python.
+It works on Python 2.4+ (include Python 3.x).
+
+import firebirdsql
+conn = firebirdsql.connect(dsn='localhost/3050:/foo/bar.fdb', user='alice', password='secret')
+cur = conn.cursor()
+cur.execute("select * from baz")
+for c in cur.fetchall():
+    print(c)
+conn.close()
+"""
 from distutils.core import setup
 import firebirdsql
 
+
 classifiers = [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Topic :: Database',
     'License :: OSI Approved :: BSD License',
 ]
 
 setup(name='firebirdsql', 
         version=firebirdsql.__version__,
-        description = 'Python dbapi for Firebird RDBMS.', 
+        description = 'Firebird RDBMS bindings for python.', 
         url='http://github.com/nakagami/pyfirebirdsql/',
         classifiers=classifiers,
         keywords=['Firebird'],
@@ -17,5 +30,5 @@ setup(name='firebirdsql',
         author='Hajime Nakagami',
         author_email='nakagami@gmail.com',
         packages = ['firebirdsql'],
+        long_description=__doc__,
 )
-
