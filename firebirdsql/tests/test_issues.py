e3c72f019574f459d4638c633b5de6890fe17287
Hajime Nakagami <nakagami@gmail.com>
2013-04-11 21:34:49 +0900

    add test_issue40, test_issue41

--- firebirdsql/tests/test_issues.py
+++ firebirdsql/tests/test_issues.py
@@ -19,9 +19,32 @@ class TestIssues(unittest.TestCase):
         cur = self.connection.cursor()
         self.assertEqual(None, cur.description)
 
-
     def test_issue_40(self):
         cur = self.connection.cursor()
+        cur.execute("SELECT RDB$INDEX_NAME FROM RDB$INDICES WHERE RDB$INDEX_NAME LIKE 'RDB$INDEX_%'")
+        self.assertNotEqual(None, cur.fetchone())
+        cur.close()
+        cur = self.connection.cursor()
+        cur.execute("SELECT RDB$INDEX_NAME FROM RDB$INDICES WHERE RDB$INDEX_NAME LIKE ?", ('RDB$INDEX_%', ))
+        self.assertNotEqual(None, cur.fetchone())
+        cur.close()
+
+    def test_issue_41(self):
+        self.connection.cursor().execute('''
+              CREATE TABLE ISSUE41
+              (
+                  a       integer,
+                  b       varchar(20)
+              )
+        ''')
+        self.connection.commit()
+
+        cur = self.connection.cursor()
+        cur.execute("insert into ISSUE41 (a, b) values (32767, 'FOO')")
+        cur.execute("insert into ISSUE41 (a, b) values (32768, 'BAR')")
+        cur.execute('''select count(*) from ISSUE41 WHERE A=?''',(32768, ))
+        self.assertEqual(cur.fetchone()[0], 1)
+        cur.close()
 
     def tearDown(self):
         self.connection.close()
