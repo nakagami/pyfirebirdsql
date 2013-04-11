817e6785bea8c71ddfaab38cb98a0919087b81ed
Hajime Nakagami <nakagami@gmail.com>
2013-04-11 22:03:46 +0900

    test_issue41

--- firebirdsql/tests/test_issues.py
+++ firebirdsql/tests/test_issues.py
@@ -31,19 +31,25 @@ class TestIssues(unittest.TestCase):
 
     def test_issue_41(self):
         self.connection.cursor().execute('''
-              CREATE TABLE ISSUE41
+              CREATE TABLE issue_41
               (
-                  a       integer,
-                  b       varchar(20)
+                  a       INTEGER,
+                  b       VARCHAR(20)
               )
         ''')
         self.connection.commit()
 
         cur = self.connection.cursor()
-        cur.execute("insert into ISSUE41 (a, b) values (32767, 'FOO')")
-        cur.execute("insert into ISSUE41 (a, b) values (32768, 'BAR')")
-        cur.execute('''select count(*) from ISSUE41 WHERE A=?''',(32768, ))
-        self.assertEqual(cur.fetchone()[0], 1)
+        cur.execute("INSERT INTO issue_41 (a, b) VALUES (32767, 'FOO')")
+        cur.execute("INSERT INTO issue_41 (a, b) VALUES (32768, 'BAR')")
+        cur.close()
+        cur = self.connection.cursor()
+        cur.execute('''SELECT b FROM issue_41 WHERE a=?''',(32767, ))
+        self.assertEqual(cur.fetchone()[0], 'FOO')
+        cur.close()
+        cur = self.connection.cursor()
+        cur.execute('''SELECT b from issue_41 WHERE a=?''',(32768, ))
+        self.assertEqual(cur.fetchone()[0], 'BAR')
         cur.close()
 
     def tearDown(self):
