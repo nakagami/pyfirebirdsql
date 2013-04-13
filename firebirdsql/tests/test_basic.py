69bfc0d5ba77e877aa4e72db5819ba43b51c5c61
Hajime Nakagami <nakagami@gmail.com>
2013-04-13 09:32:41 +0900

    test_prep

--- firebirdsql/tests/test_basic.py
+++ firebirdsql/tests/test_basic.py
@@ -4,4 +4,13 @@ import tempfile
 from firebirdsql.tests import base
 
 class TestBasic(base.TestBase):
-    pass
+    def test_prep(self):
+        cur = self.connection.cursor()
+        prep = cur.prep("select * from foo where c=?", explain_plan=True)
+        self.assertEqual(prep.sql, 'select * from foo where c=?')
+        self.assertEqual(prep.statement_type, 1)
+        self.assertEqual(prep.n_output_params, 10)
+
+        cur.execute(prep, ('C parameter', ))
+        cur.close()
+
