fa0c169eb009c66833be6405053116d70d8a94aa
Hajime Nakagami <nakagami@gmail.com>
2013-08-20 09:13:13 +0900

    bool BLR conversion

--- firebirdsql/wireprotocol.py
+++ firebirdsql/wireprotocol.py
@@ -174,6 +174,9 @@ def params_to_blr(params):
         elif p == None:
             v = bytes([0]) * 8
             blr += bytes([9, 0])
+        elif t == bool:
+            v = [1] if p else [0]
+            blr += bytes([23])
         values += v
         blr += bytes([7, 0])
         values += bytes([0]) * 4 if p != None else bytes([0xff,0xff,0x34,0x8c])
