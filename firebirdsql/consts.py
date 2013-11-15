9fbd7dec8b11de44ad2cd0b0a33fc1ad7a3e5e2e
Hajime Nakagami <nakagami@gmail.com>
2013-11-16 02:37:58 +0900

    mover charset_map from wireprotocol.py to consts.py

--- firebirdsql/consts.py
+++ firebirdsql/consts.py
@@ -338,3 +338,51 @@ isc_arg_netware = 16
 isc_arg_win32 = 17
 isc_arg_warning = 18
 isc_arg_sql_state = 19
+
+charset_map = {
+    # DB CHAR SET NAME    :   PYTHON CODEC NAME (CANONICAL)
+    # --------------------------------------------------------------------------
+    'OCTETS'              :   None, # Allow to pass through unchanged.
+    'UNICODE_FSS'         :   'utf_8',
+    'UTF8'                :   'utf_8', # (Firebird 2.0+)
+    'SJIS_0208'           :   'shift_jis',
+    'EUCJ_0208'           :   'euc_jp',
+    'DOS737'              :   'cp737',
+    'DOS437'              :   'cp437',
+    'DOS850'              :   'cp850',
+    'DOS865'              :   'cp865',
+    'DOS860'              :   'cp860',
+    'DOS863'              :   'cp863',
+    'DOS775'              :   'cp775',
+    'DOS862'              :   'cp862',
+    'DOS864'              :   'cp864',
+    'ISO8859_1'           :   'iso8859_1',
+    'ISO8859_2'           :   'iso8859_2',
+    'ISO8859_3'           :   'iso8859_3',
+    'ISO8859_4'           :   'iso8859_4',
+    'ISO8859_5'           :   'iso8859_5',
+    'ISO8859_6'           :   'iso8859_6',
+    'ISO8859_7'           :   'iso8859_7',
+    'ISO8859_8'           :   'iso8859_8',
+    'ISO8859_9'           :   'iso8859_9',
+    'ISO8859_13'          :   'iso8859_13',
+    'KSC_5601'            :   'euc_kr',
+    'DOS852'              :   'cp852',
+    'DOS857'              :   'cp857',
+    'DOS861'              :   'cp861',
+    'DOS866'              :   'cp866',
+    'DOS869'              :   'cp869',
+    'WIN1250'             :   'cp1250',
+    'WIN1251'             :   'cp1251',
+    'WIN1252'             :   'cp1252',
+    'WIN1253'             :   'cp1253',
+    'WIN1254'             :   'cp1254',
+    'BIG_5'               :   'big5',
+    'GB_2312'             :   'gb2312',
+    'WIN1255'             :   'cp1255',
+    'WIN1256'             :   'cp1256',
+    'WIN1257'             :   'cp1257',
+    'KOI8-R'              :   'koi8_r', # (Firebird 2.0+)
+    'KOI8-U'              :   'koi8_u', # (Firebird 2.0+)
+    'WIN1258'             :   'cp1258', # (Firebird 2.0+)
+}
--- firebirdsql/wireprotocol.py
+++ firebirdsql/wireprotocol.py
@@ -165,53 +165,6 @@ class WireProtocol:
     op_service_start = 85
     op_rollback_retaining = 86
 
-    charset_map = {
-    # DB CHAR SET NAME    :   PYTHON CODEC NAME (CANONICAL)
-    # --------------------------------------------------------------------------
-    'OCTETS'              :   None, # Allow to pass through unchanged.
-    'UNICODE_FSS'         :   'utf_8',
-    'UTF8'                :   'utf_8', # (Firebird 2.0+)
-    'SJIS_0208'           :   'shift_jis',
-    'EUCJ_0208'           :   'euc_jp',
-    'DOS737'              :   'cp737',
-    'DOS437'              :   'cp437',
-    'DOS850'              :   'cp850',
-    'DOS865'              :   'cp865',
-    'DOS860'              :   'cp860',
-    'DOS863'              :   'cp863',
-    'DOS775'              :   'cp775',
-    'DOS862'              :   'cp862',
-    'DOS864'              :   'cp864',
-    'ISO8859_1'           :   'iso8859_1',
-    'ISO8859_2'           :   'iso8859_2',
-    'ISO8859_3'           :   'iso8859_3',
-    'ISO8859_4'           :   'iso8859_4',
-    'ISO8859_5'           :   'iso8859_5',
-    'ISO8859_6'           :   'iso8859_6',
-    'ISO8859_7'           :   'iso8859_7',
-    'ISO8859_8'           :   'iso8859_8',
-    'ISO8859_9'           :   'iso8859_9',
-    'ISO8859_13'          :   'iso8859_13',
-    'KSC_5601'            :   'euc_kr',
-    'DOS852'              :   'cp852',
-    'DOS857'              :   'cp857',
-    'DOS861'              :   'cp861',
-    'DOS866'              :   'cp866',
-    'DOS869'              :   'cp869',
-    'WIN1250'             :   'cp1250',
-    'WIN1251'             :   'cp1251',
-    'WIN1252'             :   'cp1252',
-    'WIN1253'             :   'cp1253',
-    'WIN1254'             :   'cp1254',
-    'BIG_5'               :   'big5',
-    'GB_2312'             :   'gb2312',
-    'WIN1255'             :   'cp1255',
-    'WIN1256'             :   'cp1256',
-    'WIN1257'             :   'cp1257',
-    'KOI8-R'              :   'koi8_r', # (Firebird 2.0+)
-    'KOI8-U'              :   'koi8_u', # (Firebird 2.0+)
-    'WIN1258'             :   'cp1258', # (Firebird 2.0+)
-    }
 
     def recv_channel(self, nbytes, word_alignment=False):
         n = nbytes
@@ -234,17 +187,17 @@ class WireProtocol:
         return r[:nbytes]
 
     def str_to_bytes(self, s):
-        return s.encode(self.charset_map.get(self.charset, self.charset))
+        return s.encode(charset_map.get(self.charset, self.charset))
 
     def bytes_to_str(self, b):
         "convert bytes array to raw string"
         if PYTHON_MAJOR_VER == 3:
-            return b.decode(self.charset_map.get(self.charset, self.charset))
+            return b.decode(charset_map.get(self.charset, self.charset))
         return b
 
     def bytes_to_ustr(self, b):
         "convert bytes array to unicode string"
-        return b.decode(self.charset_map.get(self.charset, self.charset))
+        return b.decode(charset_map.get(self.charset, self.charset))
 
     def _parse_status_vector(self):
         sql_code = 0
