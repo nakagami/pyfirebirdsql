5197adb4eccab5c7aa29577bbcdb0bc9dbdded66
Hajime Nakagami <nakagami@gmail.com>
2013-12-28 15:32:52 +0900

    bytes_to_hex() in srp.py

--- firebirdsql/srp.py
+++ firebirdsql/srp.py
@@ -70,6 +70,7 @@ import sys
 import hashlib
 import hmac
 import random
+import binascii
 
 PYTHON_MAJOR_VER = sys.version_info[0]
 
@@ -219,6 +220,15 @@ def hex_to_bytes(s):
     b = bytes(ia) if PYTHON_MAJOR_VER == 3 else b''.join([chr(c) for c in ia])
     ia = [int(s[i:i+2], 16) for i in range(0, len(s), 2)]   # int array
     return bytes(ia) if PYTHON_MAJOR_VER == 3 else b''.join([chr(c) for c in ia])
+
+def bytes_to_hex(b):
+    """
+    convert bytes to hex string
+    """
+    s = binascii.b2a_hex(b)
+
+    return s
+
 
 if __name__ == '__main__':
     """
