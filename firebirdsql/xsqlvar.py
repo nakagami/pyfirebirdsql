dfc833d591078127de8af66a208662ad70ae63ec
Hajime Nakagami <nakagami@gmail.com>
2013-12-30 15:58:02 +0900

    separate to xsqlvar.py

---
+++ firebirdsql/xsqlvar.py
@@ -0,0 +1,185 @@
+##############################################################################
+# Copyright (c) 2009-2013 Hajime Nakagami<nakagami@gmail.com>
+# All rights reserved.
+# Licensed under the New BSD License
+# (http://www.freebsd.org/copyright/freebsd-license.html)
+#
+# Python DB-API 2.0 module for Firebird.
+##############################################################################
+import sys
+import datetime
+import decimal
+
+from firebirdsql.consts import *
+from firebirdsql.utils import *
+
+class XSQLVAR:
+    type_length = {
+        SQL_TYPE_VARYING: -1,
+        SQL_TYPE_SHORT: 4,
+        SQL_TYPE_LONG: 4,
+        SQL_TYPE_FLOAT: 4,
+        SQL_TYPE_TIME: 4,
+        SQL_TYPE_DATE: 4,
+        SQL_TYPE_DOUBLE: 8,
+        SQL_TYPE_TIMESTAMP: 8,
+        SQL_TYPE_BLOB: 8,
+        SQL_TYPE_ARRAY: 8,
+        SQL_TYPE_QUAD: 8,
+        SQL_TYPE_INT64: 8,
+        SQL_TYPE_BOOLEAN: 1,
+        }
+
+    type_display_length = {
+        SQL_TYPE_VARYING: -1,
+        SQL_TYPE_SHORT: 6,
+        SQL_TYPE_LONG: 11,
+        SQL_TYPE_FLOAT: 17,
+        SQL_TYPE_TIME: 11,
+        SQL_TYPE_DATE: 10,
+        SQL_TYPE_DOUBLE: 17,
+        SQL_TYPE_TIMESTAMP: 22,
+        SQL_TYPE_BLOB: 0,
+        SQL_TYPE_ARRAY: -1,
+        SQL_TYPE_QUAD: 20,
+        SQL_TYPE_INT64: 20,
+        SQL_TYPE_BOOLEAN: 5,
+        }
+
+    def __init__(self, bytes_to_str):
+        self.bytes_to_str = bytes_to_str
+        self.sqltype = None
+        self.sqlscale = None
+        self.sqlsubtype = None
+        self.sqllen = None
+        self.null_ok = None
+        self.fieldname = ''
+        self.relname = ''
+        self.ownname = ''
+        self.aliasname = ''
+
+    def io_length(self):
+        sqltype = self.sqltype
+        if sqltype == SQL_TYPE_TEXT:
+            return self.sqllen
+        else:
+            return self.type_length[sqltype]
+
+    def display_length(self):
+        sqltype = self.sqltype
+        if sqltype == SQL_TYPE_TEXT:
+            return self.sqllen
+        else:
+            return self.type_display_length[sqltype]
+
+    def precision(self):
+        return None
+
+    def __str__(self):
+        s  = '[' + str(self.sqltype) + ',' + str(self.sqlscale) + ',' \
+                + str(self.sqlsubtype) + ',' + str(self.sqllen)  + ',' \
+                + str(self.null_ok) + ',' + self.fieldname + ',' \
+                + self.relname + ',' + self.ownname + ',' \
+                + self.aliasname + ']'
+        return s
+
+    def _parse_date(self, raw_value):
+        "Convert raw data to datetime.date"
+        nday = bytes_to_bint(raw_value) + 678882
+        century = (4 * nday -1) // 146097
+        nday = 4 * nday - 1 - 146097 * century
+        day = nday // 4
+
+        nday = (4 * day + 3) // 1461
+        day = 4 * day + 3 - 1461 * nday
+        day = (day + 4) // 4
+
+        month = (5 * day -3) // 153
+        day = 5 * day - 3 - 153 * month
+        day = (day + 5) // 5
+        year = 100 * century + nday
+        if month < 10:
+            month += 3
+        else:
+            month -= 9
+            year += 1
+        return year, month, day
+
+    def _parse_time(self, raw_value):
+        "Convert raw data to datetime.time"
+        n = bytes_to_bint(raw_value)
+        s = n // 10000
+        m = s // 60
+        h = m // 60
+        m = m % 60
+        s = s % 60
+        return (h, m, s, (n % 10000) * 100)
+
+    def value(self, raw_value):
+        if self.sqltype == SQL_TYPE_TEXT:
+            return self.bytes_to_str(raw_value).rstrip()
+        elif self.sqltype == SQL_TYPE_VARYING:
+            return self.bytes_to_str(raw_value)
+        elif self.sqltype in (SQL_TYPE_SHORT, SQL_TYPE_LONG, SQL_TYPE_INT64):
+            n = bytes_to_bint(raw_value)
+            if self.sqlscale:
+                return decimal.Decimal(str(n) + 'e' + str(self.sqlscale))
+            else:
+                return n
+        elif self.sqltype == SQL_TYPE_DATE:
+            yyyy, mm, dd = self._parse_date(raw_value)
+            return datetime.date(yyyy, mm, dd)
+        elif self.sqltype == SQL_TYPE_TIME:
+            h, m, s, ms = self._parse_time(raw_value)
+            return datetime.time(h, m, s, ms)
+        elif self.sqltype == SQL_TYPE_TIMESTAMP:
+            yyyy, mm, dd = self._parse_date(raw_value[:4])
+            h, m, s, ms = self._parse_time(raw_value[4:])
+            return datetime.datetime(yyyy, mm, dd, h, m, s, ms)
+        elif self.sqltype == SQL_TYPE_FLOAT:
+            return struct.unpack('!f', raw_value)[0]
+        elif self.sqltype == SQL_TYPE_DOUBLE:
+            return struct.unpack('!d', raw_value)[0]
+        elif self.sqltype == SQL_TYPE_BOOLEAN:
+            return True if byte_to_int(raw_value[0]) != 0 else False
+        else:
+            return raw_value
+
+
+sqltype2blr = {
+    SQL_TYPE_DOUBLE: [27],
+    SQL_TYPE_FLOAT: [10],
+    SQL_TYPE_D_FLOAT: [11],
+    SQL_TYPE_DATE: [12],
+    SQL_TYPE_TIME: [13],
+    SQL_TYPE_TIMESTAMP: [35],
+    SQL_TYPE_BLOB: [9, 0],
+    SQL_TYPE_ARRAY: [9, 0],
+    SQL_TYPE_BOOLEAN: [23],
+    }
+
+def calc_blr(xsqlda):
+    "Calculate  BLR from XSQLVAR array."
+    ln = len(xsqlda) *2
+    blr = [5, 2, 4, 0, ln & 255, ln >> 8]
+    for x in xsqlda:
+        sqltype = x.sqltype
+        if sqltype == SQL_TYPE_VARYING:
+            blr += [37, x.sqllen & 255, x.sqllen >> 8]
+        elif sqltype == SQL_TYPE_TEXT:
+            blr += [14, x.sqllen & 255, x.sqllen >> 8]
+        elif sqltype == SQL_TYPE_LONG:
+            blr += [8, x.sqlscale]
+        elif sqltype == SQL_TYPE_SHORT:
+            blr += [7, x.sqlscale]
+        elif sqltype == SQL_TYPE_INT64:
+            blr += [16, x.sqlscale]
+        elif sqltype == SQL_TYPE_QUAD:
+            blr += [9, x.sqlscale]
+        else:
+            blr += sqltype2blr[sqltype]
+        blr += [7, 0]   # [blr_short, 0]
+    blr += [255, 76]    # [blr_end, blr_eoc]
+
+    # x.sqlscale value shoud be negative, so b convert to range(0, 256)
+    return bytes(256 + b if b < 0 else b for b in blr)
