7ab6715b73538b9dfde2e7a785448bd0c6d60aa0
Hajime Nakagami <nakagami@gmail.com>
2014-01-03 11:01:05 +0900

    default connect_version is 2

--- firebirdsql/fbcore.py
+++ firebirdsql/fbcore.py
@@ -455,7 +455,7 @@ class Connection(WireProtocol):
                     database=None, charset=DEFAULT_CHARSET, port=3050,
                     page_size=None, is_services=False, cloexec=False,
                     timeout=None, isolation_level=None, use_unicode=None,
-                    connect_version=3):
+                    connect_version=2):
         if dsn:
             i = dsn.find(':')
             if i < 0:
