nakagami/pyfirebirdsql

commit 02dfb473fb6bf70bca6f08f1e069c73512857f70

refactoring

firebirdsql/wireprotocol.py
@@ -11,6 +11,10 @@ import sys
 import os
 import socket
 import xdrlib, time, datetime, decimal, struct, select
+try:
+    import crypt
+except ImportError: # Not posix
+    crypt = None
 from firebirdsql.fberrmsgs import messages
 from firebirdsql import (DisconnectByPeer,
     DatabaseError, InternalError, OperationalError,
@@ -47,6 +51,9 @@ INFO_SQL_SELECT_DESCRIBE_VARS = bs([
     isc_info_sql_owner,
     isc_info_sql_alias,
     isc_info_sql_describe_end])
+
+def get_crypt(plain):
+    return crypt.crypt(plain, '9z')[2:]
 
 def convert_date(v):  # Convert datetime.date to BLR format data
     i = v.month + 9
@@ -358,9 +365,8 @@ class WireProtocol(object):
             self.client_public_key, self.client_private_key = srp.client_seed()
             specific_data = bytes_to_hex(srp.long2bytes(self.client_public_key))
         elif auth_plugin_list[0] == 'Legacy_Auth':
-            enc_pass = get_crypt(self.password)
-            if enc_pass:
-                specific_data = self.str_to_bytes(enc_pass)
+            assert crypt, "Legacy_Auth needs crypt module"
+            specific_data = self.str_to_bytes(get_crypt(self.password))
         else:
             raise OperationalError("Unknown auth plugin name '%s'" % (auth_plugin_list[0]))
         self.plugin_name = auth_plugin_list[0]

firebirdsql/utils.py
@@ -9,11 +9,6 @@
 import sys
 import binascii
 import struct
-try:
-    import crypt
-except ImportError: # Not posix
-    crypt = None
-
 
 PYTHON_MAJOR_VER = sys.version_info[0]
 
@@ -106,7 +101,3 @@ def byte_to_int(b):
     else:
         return ord(b)
 
-def get_crypt(plain):
-    if crypt is None:
-        return None
-    return crypt.crypt(plain, '9z')[2:]

commit b270537035037f410723fa84016ed55cf98ff69b

Fixing Issue #61: Commit 02dfb473fb6bf70bca6f08f1e069c73512857f70 broke Windows compatibility

firebirdsql/wireprotocol.py
@@ -53,6 +53,8 @@ INFO_SQL_SELECT_DESCRIBE_VARS = bs([
     isc_info_sql_describe_end])
 
 def get_crypt(plain):
+    if crypt is None:
+        return None
     return crypt.crypt(plain, '9z')[2:]
 
 def convert_date(v):  # Convert datetime.date to BLR format data