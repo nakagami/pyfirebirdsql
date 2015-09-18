27cdc9b2e04e1aea6298a67db8c264d9218e0f64
Hajime Nakagami <nakagami@gmail.com>
2015-09-18 12:10:19 +0900

    flake8

--- firebirdsql/__init__.py
+++ firebirdsql/__init__.py
@@ -4,10 +4,9 @@
 # Licensed under the New BSD License
 # (http://www.freebsd.org/copyright/freebsd-license.html)
 #
-# Python DB-API 2.0 module for Firebird. 
+# Python DB-API 2.0 module for Firebird.
 ##############################################################################
 
-from firebirdsql.consts import *
 
 class Error(Exception):
     def __init__(self, message, gds_codes=set(), sql_code=0):
@@ -15,39 +14,51 @@ class Error(Exception):
         self.gds_codes = gds_codes
         self.sql_code = sql_code
         self.args = [message, sql_code]
+
     def __str__(self):
         return self._message
+
 
 class InterfaceError(Error):
     pass
 
+
 class DatabaseError(Error):
     pass
 
+
 class DisconnectByPeer(Warning):
     pass
+
 
 class InternalError(DatabaseError):
     def __init__(self):
         DatabaseError.__init__(self, 'InternalError')
 
+
 class OperationalError(DatabaseError):
     pass
+
 
 class ProgrammingError(DatabaseError):
     pass
 
+
 class IntegrityError(DatabaseError):
     pass
 
+
 class DataError(DatabaseError):
     pass
+
 
 class NotSupportedError(DatabaseError):
     def __init__(self):
         DatabaseError.__init__(self, 'NotSupportedError')
 
-from firebirdsql.fbcore import ( __version__, apilevel, threadsafety, 
+
+from firebirdsql.fbcore import (
+    __version__, apilevel, threadsafety,
     paramstyle, Transaction, Cursor, Connection,
     ISOLATION_LEVEL_READ_COMMITED_LEGACY, ISOLATION_LEVEL_READ_COMMITED,
     ISOLATION_LEVEL_REPEATABLE_READ, ISOLATION_LEVEL_SNAPSHOT,
@@ -55,11 +66,13 @@ from firebirdsql.fbcore import ( __version__, apilevel, threadsafety,
     BINARY, Binary, DATE, Date,
     Time, Timestamp, DateFromTicks, TimeFromTicks, TimestampFromTicks,
 )
-
+from firebirdsql.consts import *
 import firebirdsql.services
+
 
 def connect(**kwargs):
     return Connection(**kwargs)
+
 
 def create_database(**kwargs):
     kwargs['create_new'] = True
