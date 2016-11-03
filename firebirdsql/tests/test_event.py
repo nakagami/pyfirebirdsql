c061a135f16eb06a7e00021fea36f745d6581742
Hajime Nakagami <nakagami@gmail.com>
2016-11-03 09:26:16 +0900

    modify test_event

--- firebirdsql/tests/test_event.py
+++ firebirdsql/tests/test_event.py
@@ -37,13 +37,20 @@ class TestEvent(TestBase):
                 post_event 'event_c';
                 post_event 'event_a';
             END''')
+        cur.execute('''
+            CREATE TRIGGER trig_test_delete_event
+                FOR test_table
+                    after delete
+            AS BEGIN
+                post_event 'event_d';
+            END''')
         conn.commit()
         conn.close()
 
     def tearDown(self):
         pass
 
-    def _produce(self):
+    def _insert(self):
         conn = firebirdsql.connect(
                 auth_plugin_name=self.auth_plugin_name,
                 wire_crypt=self.wire_crypt,
@@ -54,6 +61,20 @@ class TestEvent(TestBase):
                 password=self.password)
         cur = conn.cursor()
         cur.execute('insert into test_table values (1)')
+        conn.commit()
+        conn.close()
+
+    def _delete(self):
+        conn = firebirdsql.connect(
+                auth_plugin_name=self.auth_plugin_name,
+                wire_crypt=self.wire_crypt,
+                host=self.host,
+                port=self.port,
+                database=self.database,
+                user=self.user,
+                password=self.password)
+        cur = conn.cursor()
+        cur.execute('delete from test_table')
         conn.commit()
         conn.close()
 
@@ -68,19 +89,23 @@ class TestEvent(TestBase):
                 password=self.password)
         conduit = conn.event_conduit(['event_a', 'event_b', 'event_d'])
         result = conduit.wait()
-        assert result == {'event_b': 1, 'event_a': 2, 'event_d': 0}
+        # 1 record is inserted
         self.assertEqual(result, {'event_b': 1, 'event_a': 2, 'event_d': 0})
     
-        self._produce()
-    
-        while result == {'event_b': 0, 'event_a': 0, 'event_d': 0}:
-            result = conduit.wait()
+        self._insert()
+        result = conduit.wait()
+        # 1 more record is inserted
         self.assertEqual(result,  {'event_b': 1, 'event_a': 2, 'event_d': 0})
+
+        self._delete()
+        result = conduit.wait()
+        # 2 records are deleted
+        self.assertEqual(result,  {'event_b': 0, 'event_a': 0, 'event_d': 2})
     
         conduit.close()
         conn.close()
 
     def test_event(self):
         thread.start_new_thread(self._handle_event, ())
-        time.sleep(3)
-        self._produce()
+        time.sleep(1)
+        self._insert()
