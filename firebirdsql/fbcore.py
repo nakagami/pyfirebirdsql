c633533533e652ea4d6ab936ce0a23d8e1cd88ae
Hajime Nakagami <nakagami@gmail.com>
2018-02-14 23:28:07 +0900

    revert drop statement

--- firebirdsql/fbcore.py
+++ firebirdsql/fbcore.py
@@ -283,8 +283,9 @@ class Cursor(object):
         if isinstance(query, PreparedStatement):
             stmt = query.stmt
         else:
-            if self.stmt and self.stmt.stmt_type == isc_info_sql_stmt_select:
-                self.stmt.close()
+            if self.stmt:
+                self.stmt.drop()
+                self.stmt = None
             if self.stmt is None:
                 self.stmt = Statement(self.transaction)
             stmt = self.stmt
