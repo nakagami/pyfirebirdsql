212a0cc73ed0fc44d6be3d241323e85de8045d72
Hajime Nakagami <nakagami@gmail.com>
2025-01-09 12:30:32 +0900

    update copyright year

--- firebirdsql/chacha.py
+++ firebirdsql/chacha.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2021-2024 Hajime Nakagami<nakagami@gmail.com>
+# Copyright (c) 2021-2025 Hajime Nakagami<nakagami@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
