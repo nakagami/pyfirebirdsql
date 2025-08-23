97343cd4a6405bf65e8979d02b5164f144771f0e
Hajime Nakagami <nakagami@gmail.com>
2025-08-23 07:58:24 +0000

    update copyright year

--- firebirdsql/event_conduit.py
+++ firebirdsql/event_conduit.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2009-2024, Hajime Nakagami<nakagami@gmail.com>
+# Copyright (c) 2009-2025, Hajime Nakagami<nakagami@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
--- firebirdsql/utils.py
+++ firebirdsql/utils.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2014-2024, Hajime Nakagami<nakagami@gmail.com>
+# Copyright (c) 2014-2025, Hajime Nakagami<nakagami@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
