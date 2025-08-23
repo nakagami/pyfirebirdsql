be5abb896f3f668145246c8ff829aa86451ca68b
Hajime Nakagami <nakagami@gmail.com>
2025-08-23 08:11:58 +0000

    update copyright year

--- firebirdsql/consts.py
+++ firebirdsql/consts.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2011-2023, Hajime Nakagami<nakagami@gmail.com>
+# Copyright (c) 2011-2025, Hajime Nakagami<nakagami@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
